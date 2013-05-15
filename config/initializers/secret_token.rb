f1155ef80a50368de54bc71cced66b0e39ad580c
Rafael Pereira <rafaelbsides@gmail.com>
2013-05-15 18:50:09 -0300

    Starting fresh

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NhamCo::Application.config.secret_token = '1572fa94393eb43906096f0cc8164a8048a69d010796cd14747d7df92aeb68749c674e8b8fb5a533e7d18abce80a5b4fecc943115e0d8c73bf34163a50979d33'
