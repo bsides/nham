c34d4a57bd6e83d73ffb245b0092436977ffb528
Rafael Pereira <rafaelbsides@gmail.com>
2013-05-16 03:21:07 -0300

    the test was so successful in ajaxifying the stuff i used it as an example to mockup a fully working mobile app... well, kind of

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,5 +1,14 @@
 class CalcsController < ApplicationController
+
 	def create
-  	@sum = eval(params[:data])
-	end
+  	@carbs = params[:carbs].to_f
+  	@protein = params[:protein].to_f
+  	@fat = params[:fat].to_f
+  	@fiber = params[:fiber].to_f
+
+
+  	formula = (@carbs * 16) + (@protein * 19) + (@fat * 45) - (@fiber * 14)
+  	@result = (formula / 175).round(2)
+  end
+  
 end
--- app/helpers/calcs_helper.rb
+++ app/helpers/calcs_helper.rb
@@ -1,2 +1,13 @@
 module CalcsHelper
+
+	def hinted_text_field_tag(name, value = nil, hint = "Click and enter text", options={})
+  value = value.nil? ? hint : value
+  text_field_tag name, value, {:onclick => "if($(this).value == '#{hint}'){$(this).value = ''}", :onblur => "if($(this).value == ''){$(this).value = '#{hint}'}" }.update(options.stringify_keys)
+	end
+
+	# inside form_for example
+
+	#hinted_text_field_tag :search, params[:search], "Enter name, brand or mfg.", :size => 30  
+	# => <input id="search" name="search" onblur="if($(this).value == ''){$(this).value = 'Enter name, brand or mfg.'}" onclick="if($(this).value == 'Enter name, brand or mfg.'){$(this).value = ''}" size="30" type="text" value="Enter name, brand or mfg." />
+
 end
